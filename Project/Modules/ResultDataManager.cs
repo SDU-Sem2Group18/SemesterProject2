a194b5c033debdd8317b6104a4197d634b5e9973
Nick <niklas.braun30419@gmail.com>
2024-04-14 23:26:18 +0200

    KAN-28 Further Changes to ResultDataManager

--- Project/Modules/ResultDataManager.cs
+++ Project/Modules/ResultDataManager.cs
@@ -1,33 +1,63 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
 
 namespace Project.Modules
 {
     
-    public class ResultData {
-        public int unitId { get; set; }
+    public struct ResultData {
+        [Index(0)]
+        [Name("Unit Name")]
+        public AssetManager.UnitNames unitName { get; set; }
+
+        [Index(1)]
+        [Name("Produced Heat (MWh)")]
         public float producedHeat { get; set; }
+
+        [Index(2)]
+        [Name("Produced Electricity (MWh)")]
         public float producedElectricity { get; set; }
+
+        [Index(3)]
+        [Name("Consumed Electricity (MWh)")]
         public float consumedElectricity { get; set; }
-        public float productionCost { get; set; }
+
+        [Index(4)]
+        [Name("Energy Consumed (MWh)")]
         public float energyConsumed { get; set; }
+        
+        
+        [Index(5)]
+        [Name("CO2 Emissions (tonnes)")]
         public float CO2Emissions { get; set; }
+        
+        
+        
+        [Index(6)]
+        [Name("Production Cost (Kr)")]
+        public float productionCost { get; set; }
+        
+        
+        [Index(7)]
+        [Name("Profit (Kr)")]
+        public float profit { get; set; }
     }
 
     public class ResultDataManger {
         private List<ResultData> resultDataList;
 
-        public void StoreResultData(ResultData data){
+        public void SaveResultData(string path){
             
             //Implement The logic to store ResultData
             resultDataList.Add(data);
         }
 
-        public ResultData RetrieveResltData(int unitId){
+        public ResultData RetrieveResultData(AssetManager.UnitNames unitName){
             // Implement the logic to retrieve a specific ResultData by unitId
-            return resultDataList.Find(data => data.unitId == unitId);
+            return resultDataList.Find(data => data.unitName == unitName);
         }
 
         public List<ResultData> RetrieveAllResultData(){

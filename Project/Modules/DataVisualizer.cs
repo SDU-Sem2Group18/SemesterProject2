e5e034c8ed079541329026a4728bc14b1c0b40a0
Norrokas <rokutis.norbutas@gmail.com>
2024-04-15 16:10:23 +0200

    KAN-26 Implemented Source Data Graphs
    Co-authored-by: Nick <nicki419@users.noreply.github.com>

--- Project/Modules/DataVisualizer.cs
+++ Project/Modules/DataVisualizer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ScottPlot;
 
 namespace Project.Modules   
 {
@@ -39,40 +40,87 @@ namespace Project.Modules
 
     }
 
-    public enum DataType{
-        //Simple place holders
-        Type1,
-        Type2,
-        Type3
-    }
-
-    public class DataVisualizationManager
+    public class DataVisualizationManager: IDisposable
     {
-        private Dictionary<DataType, Visualization> visualizations;
+        // Constructor
+        public DataVisualizationManager() {}
 
-        public DataVisualizationManager()
+        public void CreatePlotVisualization(List<double> xData, List<double> yData, string filePath, string title, string xName, string yName)
         {
-            visualizations = new Dictionary<DataType, Visualization>();
-        }
+            var plt = new ScottPlot.Plot();
 
-        public void CreateVisualization(DataType type){
+            var scatter = plt.Add.Scatter(xData.ToArray(), yData.ToArray());
+            plt.Title(title);
+            plt.XLabel(xName);
+            plt.YLabel(yName);
+            plt.Axes.DateTimeTicksBottom();
 
-            // Logic to create a visualization based on the DataType
-            // Example instantiation - we would have our own logic to define Name and ImagePath
-            var visualization = new Visualization($"Visualization for {type}", $"path/to/image/for/{type}");
-            visualizations[type] = visualization;
-        }
-
-        public Visualization GetVisualization(DataType type){
+            // Minimum Data Calculation
+            double min = yData[0];
+            foreach(double _ in yData) if(_ < min) min = _;
+            var minLine = plt.Add.HorizontalLine(min);
+            minLine.Color = ScottPlot.Color.FromHex("4fb051");
+            minLine.LinePattern = ScottPlot.LinePattern.Dashed;
+            minLine.LabelStyle.Text = "Minimum";
+            minLine.LabelStyle.OffsetX = 1100f;
+            minLine.LabelStyle.OffsetY = -7.5f;
+            //minLine.Axes.YAxis = plt.Axes.Right;
             
-            visualizations.TryGetValue(type, out var visualization);
-            return visualization;
+
+            // Maximum Data Calculation
+            double max = yData[0];
+            foreach(double _ in yData) if(_ > max) max = _;
+            var maxLine = plt.Add.HorizontalLine(max);
+            maxLine.Color = ScottPlot.Color.FromHex("bf1123");
+            maxLine.LinePattern = ScottPlot.LinePattern.Dashed;
+            maxLine.LabelStyle.Text = "Maximum";
+            maxLine.LabelStyle.OffsetX = 1100f;
+            maxLine.LabelStyle.OffsetY = 7.5f;
+            //maxLine.Axes.YAxis = plt.Axes.Right;
+            
+
+            // Median Calculation
+            double median = 0;
+            foreach(double _ in yData) median += _;
+            median /= yData.Count();
+            var medianLine = plt.Add.HorizontalLine(median);
+            medianLine.Color = ScottPlot.Color.FromHex("deb03c");
+            medianLine.LinePattern = ScottPlot.LinePattern.Dashed;
+            medianLine.LabelStyle.Text = "Median";
+            medianLine.LabelStyle.OffsetX = 1100f;
+            //medianLine.Axes.YAxis = plt.Axes.Right;
+
+            //plt.ShowLegend(Alignment.UpperRight);
+            plt.SavePng(filePath,1200, 600);
+
+            // Create a new Visualization with the path to the new plot image
+            var visualization = new Visualization("Visualization", filePath);
+            visualization.Display();
         }
 
-        public void UpdateVisualization(DataType type, Visualization newData){
+        // Assuming that you have a method to get X and Y data from SourceDataManager, it might look like this:
+        public void CreateVisualisationFromSourceData(SourceDataManager sourceDataManager)
+        {
+            var heatDataList = sourceDataManager.GetHeatData();
 
-            if(visualizations.ContainsKey(type)) visualizations[type] = newData;
+            // Extracting the x and y data from heatDataList
+            List<double> xData = heatDataList.Select(data => data.TimeFrom.ToOADate()).ToList();
+            List<double> yData = heatDataList.Select(data => (double)data.HeatDemand).ToList();
+            string title = "Heat Demand";
+            CreatePlotVisualization(xData, yData, @"Data/HeatDemand.png", title, "Time", "Heat Demand (MWh)");
 
+            yData = heatDataList.Select(data => (double)data.ElectricityPrice).ToList();
+            title = "Electricity Price";
+            CreatePlotVisualization(xData, yData, @"Data/ElectricityPrice.png", title, "Time", "Electricity Price (Kr)");
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected bool Disposed {get; private set; }
+        protected virtual void Dispose(bool disposing) {
+            Disposed = true;
         }
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -5,5 +5,10 @@ namespace Project;
 public class Program {
     public static void Main() {
         Console.WriteLine("Hello World!");
+    
+        using(SourceDataManager sourceDataManager = new SourceDataManager(@"Data/summer.csv"))
+        using(DataVisualizationManager dataVisualizationManager = new DataVisualizationManager()) {
+            dataVisualizationManager.CreateVisualisationFromSourceData(sourceDataManager);
+        }
     }
 }

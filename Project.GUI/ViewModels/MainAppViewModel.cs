154892c270bc3f8bb4b1a6e1ce8cf685fc5a66cb
Nick <niklas.braun30419@gmail.com>
2024-04-27 17:22:50 +0200

    Added remaining MainApp Views and linked to buttons

--- Project.GUI/ViewModels/MainAppViewModel.cs
+++ Project.GUI/ViewModels/MainAppViewModel.cs
@@ -2,13 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ReactiveUI;
 
 namespace Project.GUI.ViewModels
 {
     public class MainAppViewModel : ViewModelBase
     {
+        private ViewModelBase _contentViewModel;
         public MainAppViewModel() {
-
+            GridUnit = new GridUnitViewModel();
+            SourceData = new SourceDataViewModel();
+            Optimiser = new OptimiserViewModel();
+            
+            _contentViewModel = GridUnit;
         }
+        public GridUnitViewModel GridUnit { get; }
+        public SourceDataViewModel SourceData { get; }
+        public OptimiserViewModel Optimiser { get; }
+
+        public ViewModelBase ContentViewModel {
+            get => _contentViewModel;
+            set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+        }
+
+        public void GridUnitButton() {
+            ContentViewModel = GridUnit;
+        }
+
+        public void SourceDataButton() {
+            ContentViewModel = SourceData;
+        }
+
+        public void OptimiserButton() {
+            ContentViewModel = Optimiser;
+        }
+
     }
 }
---
+++ Project.GUI/Views/OptimiserView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Project.GUI.Views;
+
+public partial class OptimiserView : UserControl
+{
+    public OptimiserView()
+    {
+        InitializeComponent();
+    }
+}

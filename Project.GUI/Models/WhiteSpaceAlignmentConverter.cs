0cfca7b6502345fa7d142721471be4bb7cd3aa11
Nick <niklas.braun30419@gmail.com>
2024-04-28 22:21:47 +0200

    Added Fonts, Used Monospace Font for Source Data Table, added WhiteSpace Converter to properly align Source Data

---
+++ Project.GUI/Models/WhiteSpaceAlignmentConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+
+namespace Project.GUI.Models
+{
+    public class WhiteSpaceAlignmentConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        if (value is float)
+        {
+            float number = (float)value;
+            int intParameter;
+            Int32.TryParse(parameter.ToString(), out intParameter);
+            string returnString = string.Format("{0:0.00}", number);
+            return (returnString.Length == intParameter - 1) ?  $"  {returnString}" : returnString;
+        }
+        return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}

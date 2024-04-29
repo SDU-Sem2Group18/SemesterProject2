219f9cac4c1539c34bb83bf9849cd92ad729b4aa
Nick <niklas.braun30419@gmail.com>
2024-04-29 11:22:08 +0200

    Fixed WhiteSPaceAlignmentConverter Warnings

--- Project.GUI/Models/WhiteSpaceAlignmentConverter.cs
+++ Project.GUI/Models/WhiteSpaceAlignmentConverter.cs
@@ -7,19 +7,19 @@ using Avalonia.Data.Converters;
 namespace Project.GUI.Models
 {
     public class WhiteSpaceAlignmentConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture) {
         if (value is float)
         {
             float number = (float)value;
             int intParameter;
-            Int32.TryParse(parameter.ToString(), out intParameter);
+            Int32.TryParse(parameter?.ToString(), out intParameter);
             string returnString = string.Format("{0:0.00}", number);
             return (returnString.Length == intParameter - 1) ?  $"  {returnString}" : returnString;
         }
-        return value;
+        return value!;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture) {
             throw new NotImplementedException();
         }
     }
